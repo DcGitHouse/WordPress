cc584e661be6556ff376d3cfe0cd8175db5018a7
Jon Howard <jon.showard@gmail.com>
2015-10-02 08:47:40 -0230

    updated WP core

---
+++ media/sucuri/sucuri-integrity.php
@@ -0,0 +1,6 @@
+<?php
+// datastore=integrity;
+// created_on=1443720787;
+// updated_on=1443720787;
+exit(0);
+?>
---
+++ media/sucuri/sucuri-lastlogins.php
@@ -0,0 +1,3 @@
+<?php exit(0); ?>
+{"user_id":1,"user_login":"dcdesign","user_remoteaddr":"192.168.13.1","user_hostname":"192.168.13.1","user_lastlogin":"2015-10-01 15:03:46"}
+{"user_id":1,"user_login":"dcdesign","user_remoteaddr":"192.168.13.1","user_hostname":"192.168.13.1","user_lastlogin":"2015-10-01 15:11:46"}
